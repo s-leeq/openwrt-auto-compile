f7a33a2aa2ba390b61f13437b7c3ef11d4f26a52
s-leeq <43868433+s-leeq@users.noreply.github.com>
2022-11-04 15:09:37 +0800

    移动文件

--- devices/x86/diy-part1.sh
+++ devices/x86/diy-part1.sh
@@ -10,16 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
-# echo 'src-git geodata https://github.com/sbwml/v2ray-geodata' >>feeds.conf.default
-
 mkdir package/luci-app-openclash
 cd package/luci-app-openclash
 git init
